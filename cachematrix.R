f99336317fade2a736f0ff0f704d6e845d1f5e4d
awimberl <awimberl@gmail.com>
2014-10-26 10:24:56 -0500

    Implemented makeCahceMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a matrix and sets up properties (gets and sets)
+## that will be used to cache a matrix
 
-## Write a short comment describing this function
+## This function creates an R object that can be used to cache the solve() function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function checks if x is null. If it is, it performs solve()
+## Otherwise, it returns a cached result of solve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
